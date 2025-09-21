4eafce469ad64710025a436d17e81fd2c0020cfd
NoobCoder-5002 <deboprovosarkar@gmail.com>
2025-09-21 21:00:30 +0530

    Traffic_Light_Simulator File/code Added

---
+++ Traffic_Light_Simulator/Traffic_Light_Simulator.ino
@@ -0,0 +1,30 @@
+#define RED 1
+#define YELLOW 2
+#define GREEN 3
+
+void setup() {
+    pinMode(RED,OUTPUT);
+    pinMode(YELLOW,OUTPUT);
+    pinMode(GREEN,OUTPUT);
+}
+
+void loop() {
+    digitalWrite(RED,1);
+    digitalWrite(YELLOW,0);
+    digitalWrite(GREEN,0);
+    
+    delay(5000);
+    
+    digitalWrite(RED,0);
+    digitalWrite(YELLOW,1);
+    digitalWrite(GREEN,0);
+    
+    delay(2500);
+    
+    digitalWrite(RED,0);
+    digitalWrite(YELLOW,0);
+    digitalWrite(GREEN,1);
+    
+    delay(5000);
+    
+}
