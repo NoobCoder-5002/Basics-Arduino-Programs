9c6b663674ad3fb303ea1d4f46ab1a014765b16f
NoobCoder-5002 <deboprovosarkar@gmail.com>
2025-09-22 08:02:20 +0530

    Modified Multiple LED

--- Multiple_LED/Multiple_LED.ino
+++ Multiple_LED/Multiple_LED.ino
@@ -1,20 +1,20 @@
 
-#define LED1 1
-#define LED2 2
-#define LED3 3
-#define LED4 4
-#define LED5 5
-#define LED6 6
-#define LED7 7
+#define LED1 2
+#define LED2 3
+#define LED3 4
+#define LED4 5
+#define LED5 6
+#define LED6 7
+#define LED7 8
 
 void setup() {
-  pinMode(1,OUTPUT);
   pinMode(2,OUTPUT);
   pinMode(3,OUTPUT);
   pinMode(4,OUTPUT);
   pinMode(5,OUTPUT);
   pinMode(6,OUTPUT);
   pinMode(7,OUTPUT);
+  pinMode(8,OTUPUT):
 }
 
 void loop() {
